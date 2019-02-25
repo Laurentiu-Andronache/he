a154c27fe4da072a0d75d7d324eb77afbeaaa374
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-02-25 00:05:37 +0000

    add decorator debug() - outputs to logging.DEBUG the function arguments and return value

--- src/he/decorators.py
+++ src/he/decorators.py
@@ -14,7 +14,48 @@ def timer(func):
         value = func(*args, **kwargs)
         end = perf_counter()
         _logger = logging.getLogger(__name__ + '.' + func.__name__)
-        _logger.debug('runtime: {:.4f} seconds'.format(end - start))
+        _logger.debug(' runtime: {:.4f} seconds'.format(end - start))
         return value
 
     return wrapper
+
+
+def debug(func):
+    """Outputs to logging.DEBUG the function arguments and return value."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f'{k}={v!r}' for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        _logger = logging.getLogger(__name__ + '.' + func.__name__)
+        _logger.debug(' called with args (%s)', signature)
+        value = func(*args, **kwargs)
+        _logger.debug(f' returned %r', value)
+        return value
+
+    return wrapper
+
+
+@debug
+def enjoy_decorators():
+    """Enjoy seeing what the decorators from this file in action."""
+
+    from time import sleep
+
+    sleep = debug(sleep)
+
+    @timer
+    def sleep_times(time, times):
+        val = time * times
+        sleep(val)
+        return val
+
+    sleep_times(0.1, 2)
+
+    return 1337
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    exit(enjoy_decorators())
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from he.decorators import timer
+from he.decorators import timer, debug
 
 
 def test_timer(caplog):
@@ -8,8 +8,25 @@ def test_timer(caplog):
 
     @timer
     def sleep_one_second():
-        sleep(0.01)
+        sleep(0.1)
 
     sleep_one_second()
     assert sleep_one_second.__name__ == 'sleep_one_second'
-    assert '0.01' in caplog.text
+    assert caplog.record_tuples
+
+    logged_message = caplog.messages[0]
+    time_str = logged_message.split()[1]
+    time_rounded = round(float(time_str), 2)
+    assert 0.1 == time_rounded
+
+
+def test_debug(caplog):
+    caplog.set_level(0)
+
+    @debug
+    def add(val1, val2=855):
+        return val1 + val2
+
+    add(482)
+    assert caplog.messages[0] == ' called with args (482)'
+    assert caplog.messages[1] == ' returned 1337'
