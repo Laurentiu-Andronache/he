30ef5dd544581e9c7aba16ce183aefaf17db199f
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-03-10 15:15:00 +0000

    improve tip text

--- src/he/http_tools.py
+++ src/he/http_tools.py
@@ -9,7 +9,7 @@ import requests
 
 _LOGGER = logging.getLogger(__name__)
 
-# from https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
+# user agents can be downloaded from https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
 DESKTOP_AGENTS = open_text('he.data', 'user_agents.txt').readlines()
 
 
