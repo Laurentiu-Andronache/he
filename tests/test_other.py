5021bf902a92ac952a9ff3e4f7b7968ae154ef6e
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-03-10 15:12:08 +0000

    make compatible with `dir` linux command return value

--- tests/test_other.py
+++ tests/test_other.py
@@ -2,5 +2,7 @@ from he.other import run
 
 
 def test_run():
-    assert 1 == run('non-existent-command', capture_output=True)
+    ret_err = run('non-existent-command', capture_output=True)
+    assert isinstance(ret_err, int)
+    assert ret_err != 0
     assert 0 == run('dir', capture_output=True)
